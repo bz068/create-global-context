6e60a82520133773f89003e4b411e8f6c7e83c42
b zazai <baghdadzazai@yahoo.com>
2022-10-25 16:31:16 +0100

    init rollup

---
+++ rollup.config.js
@@ -0,0 +1,49 @@
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import typescript from 'rollup-plugin-typescript2';
+import babel from '@rollup/plugin-babel';
+import pkg from './package.json';
+
+const extensions = ['.js', '.jsx', '.es6', '.es', '.mjs', '.ts', '.tsx'];
+
+const presets = [
+  [
+    '@babel/preset-env',
+    {
+      modules: false,
+    },
+  ],
+  '@babel/preset-react',
+  '@babel/preset-typescript',
+];
+export default {
+  input: 'src/index.tsx',
+  output: [
+    {
+      file: pkg.main,
+      format: 'cjs',
+    },
+    {
+      file: pkg.module,
+      format: 'esm',
+    },
+  ],
+  plugins: [
+    commonjs(),
+    resolve({
+      extensions,
+    }),
+    typescript({
+      tsconfigOverride: {
+        exclude: ['__tests__', 'src/**/*.test.ts', 'src/**/*.test.tsx'],
+      },
+    }),
+    babel({
+      extensions,
+      babelHelpers: 'bundled',
+      presets,
+      exclude: 'node_modules/**',
+    }),
+  ],
+  external: [...Object.keys(pkg.devDependencies || {}), ...Object.keys(pkg.peerDependencies || {})],
+};
