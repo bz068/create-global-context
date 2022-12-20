a9445d0d7c2518d5a9dda96abb0a53eaeb881f23
b zazai <baghdadzazai@yahoo.com>
2022-12-20 19:01:38 +0000

    test reducer functionality

--- tests/useStoreData.test.ts
+++ tests/useStoreData.test.ts
@@ -1,6 +1,7 @@
 import { describe, expect, test, beforeEach } from '@jest/globals';
 import React from 'react';
 import { useStoreData } from '../src/useStoreData';
+import { reducer } from './createStore.test';
 
 describe('useStoreData', () => {
   const state = { count: 1, name: 'Context' };
@@ -23,6 +24,15 @@ describe('useStoreData', () => {
     const store = useStoreData(state);
     store.subscribe((newState) => (state = newState));
     store.publish((state) => ({ count: state.count + 1 }));
+    expect(state).toEqual({ count: 2, name: 'Context' });
+    expect(store.getState()).toEqual({ count: 2, name: 'Context' });
+  });
+
+  test('subscribe and publish should be working as expected with reducer - useStoreData', () => {
+    let state;
+    const store = useStoreData(state, reducer);
+    store.subscribe((newState) => (state = newState));
+    store.publishToReducer({ type: 'increment' });
     expect(state).toEqual({ count: 2, name: 'Context' });
     expect(store.getState()).toEqual({ count: 2, name: 'Context' });
   });
