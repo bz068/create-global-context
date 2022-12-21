7f21c4240b3d0f6ad971f9da472bd26f74158002
b zazai <baghdadzazai@yahoo.com>
2022-12-21 01:11:23 +0000

    add demo for reducer

--- examples/demo-reducer/src/App.tsx
+++ examples/demo-reducer/src/App.tsx
@@ -1,10 +1,34 @@
 import './App.css';
 import { createGlobalContext } from 'create-global-context';
-const { Provider, useStore } = createGlobalContext({
-  name: 'Context',
-  id: 1,
-  count: 1,
-});
+
+type State = {
+  count: number;
+  name: string;
+};
+
+type Action = { type: 'increment' } | { type: 'decrement' } | { type: 'reset'; payload: number };
+
+const reducer = (state: State, action: Action) => {
+  switch (action.type) {
+    case 'increment':
+      return { ...state, count: state.count + 1 };
+    case 'decrement':
+      return { ...state, count: state.count - 1 };
+    case 'reset':
+      return { ...state, count: action.payload };
+    default:
+      throw new Error();
+  }
+};
+
+const { Provider, useStore, useDispatch } = createGlobalContext(
+  {
+    name: 'Context',
+    id: 1,
+    count: 1,
+  },
+  reducer,
+);
 
 export default function App() {
   return (
@@ -13,6 +37,9 @@ export default function App() {
         <NameComponent />
         <Count />
         <JustCount />
+        <hr />
+        <p>useDispatch</p>
+        <CountAdder />
       </div>
     </Provider>
   );
@@ -23,13 +50,17 @@ const NameComponent = () => {
   return <div>{name}</div>;
 };
 const Count = () => {
-  const [count, setStore] = useStore((state) => state.count);
+  const [count, dispatch] = useStore((state) => state.count);
   return (
     <div>
       <p id="count-component">{count}</p>
-      <button onClick={() => setStore((currentState) => ({ count: currentState.count + 1 }))}>ADD</button>
+      <button onClick={() => dispatch({ type: 'increment' })}>ADD</button>
     </div>
   );
+};
+const CountAdder = () => {
+  const dispatch = useDispatch();
+  return <button onClick={() => dispatch({ type: 'increment' })}>ADD</button>;
 };
 
 const JustCount = () => {
